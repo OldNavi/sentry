3294766f2f85d95dbbe966675cbc7da4ec60a300
Yagiz Nizipli <yagiz@nizipli.com>
2024-02-29 12:19:36 -0500

    build: test redis-cluster on CI

--- src/sentry/testutils/helpers/__init__.py
+++ src/sentry/testutils/helpers/__init__.py
@@ -4,6 +4,7 @@ from .features import *  # NOQA
 from .link_header import *  # NOQA
 from .options import *  # NOQA
 from .query import *  # NOQA
+from .redis import *  # NOQA
 from .slack import *  # NOQA
 from .socket import *  # NOQA
 from .task_runner import *  # NOQA
